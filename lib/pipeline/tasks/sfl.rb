53fd5232d3edb2a413681703d8b03a1abf6781dc
Rafa Pérez <rafael.perez@groupon.com>
2015-07-22 13:03:10 -0300

    initialized Sensitive file scan

---
+++ lib/pipeline/tasks/sfl.rb
@@ -0,0 +1,67 @@
+require 'pipeline/tasks/base_task'
+require 'json'
+require 'pipeline/util'
+require 'find'
+
+class Pipeline::SFL < Pipeline::BaseTask
+  
+  Pipeline::Tasks.add self
+  include Pipeline::Util
+  
+  def initialize(trigger)
+    super(trigger)
+    @name = "SFL"
+    @description = "Sentive Files Lookup"
+    @stage = :code
+    @labels << "code"
+    Pipeline.debug "initialized SFL"
+    @patterns = read_patterns_file!
+  end
+  
+  def run
+    Pipeline.notify "#{@name}"
+    @files = Find.find(@trigger.path)
+    Pipeline.debug "Found #{@files.size} files"
+  end
+
+  def analyze
+    begin
+      @files.each do |file|
+        @patterns.each do |pattern|
+          case pattern['part']
+            when 'filename'
+              if foo(File.basename(file), pattern)
+                report 'filename', pattern['description'], file, 'unknown', 'TBD'
+              end
+            when 'extension'
+              if foo(File.extname(file), pattern)
+                report 'extension', pattern['description'], file, 'unknown', 'TBD'
+              end
+          end
+        end
+      end
+    rescue Exception => e
+      Pipeline.warn e.message
+    end
+  end
+
+  def supported?
+    true
+  end
+
+  def foo(txt, pattrn)
+    case pattrn['type']
+      when 'match'
+        return txt == pattrn['pattern']
+      when 'regex'
+        regex = Regexp.new(pattrn['pattern'], Regexp::IGNORECASE)
+        return !regex.match(txt).nil?
+    end  
+  end
+
+  def read_patterns_file!
+    JSON.parse(File.read("#{File.dirname(__FILE__)}/../../../patterns.json"))
+  rescue JSON::ParserError => e
+    Pipeline.warn "Cannot parse pattern file: #{e.message}"
+  end
+end
