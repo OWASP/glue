75445d99bdac1dad47b93656e164233bd4abd198
Matt Konda <mkonda@jemurai.com>
2016-01-02 09:10:55 -0600

    Use jemurai docker location temporarily until OWASP is available.

--- docker/pipeline/build.sh
+++ docker/pipeline/build.sh
@@ -1,2 +1,3 @@
-#docker build -f Dockerfile -t jemurai/pipeline:0.6 .
-docker build -no-cache -f Dockerfile -t jemurai/pipeline:0.7 .
+#docker build -f Dockerfile -t owasp/pipeline:0.6 .
+# Planning to update to OWASP as soon as access granted.
+docker build -no-cache -f Dockerfile -t jemurai/pipeline:0.8 .
