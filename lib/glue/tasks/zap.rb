b61a98c7ad640f71ef69de40af948d35f63e68dc
pardotkynes <support@mimiran.com>
2017-04-20 17:31:17 -0500

    Support ZAP API token, better error message

--- lib/glue/tasks/zap.rb
+++ lib/glue/tasks/zap.rb
@@ -86,9 +86,15 @@ class Glue::Zap < Glue::BaseTask
   end
 
   def supported?
+    apikey = "#{@tracker.options[:zap_api_token]}"
     base = "#{@tracker.options[:zap_host]}:#{@tracker.options[:zap_port]}"
-    supported=JSON.parse(Curl.get("#{base}/JSON/core/view/version/").body_str)
-    if supported["version"] =~ /2.(4|5).\d+/
+    begin
+      supported=JSON.parse(Curl.get("#{base}/JSON/core/view/version/?apikey=#{apikey}").body_str)
+    rescue Exception => e
+      Glue.error "#{e.message}. Tried to connect to #{base}/JSON/core/view/version/. Check that ZAP is running on the right host and port and that you have the appropriate API key, if required."
+      return false
+    end
+    if supported["version"] =~ /2.(4|5|6).\d+/
       return true
     else
       Glue.notify "Install ZAP from owasp.org and ensure that the configuration to connect is correct.  Supported versions = 2.4.0 and up - got #{supported['version']}"
