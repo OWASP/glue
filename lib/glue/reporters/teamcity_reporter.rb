f1396eb9911fbde4fba5c922a20b6b550ee67625
Omer Levi Hevroni <omerl@soluto.com>
2017-07-24 11:51:23 +0300

    added teamcity reporter

--- lib/glue/reporters/teamcity_reporter.rb
+++ lib/glue/reporters/teamcity_reporter.rb
@@ -14,37 +14,32 @@ class Glue::TeamCityReporter < Glue::BaseReporter
 
     if (tracker.options[:teamcity_min_level])
       unless tracker.options[:teamcity_min_level].is_a? Integer
-        Glue.fatal "min level should be a number, got: #{tracker.options[:teamcity_min_level]}"
+        Glue.fatal "min level should be a number, got: #{tracker.options[:teamcity-min-level]}"
       end
-      unless tracker.options[:teamcity_min_level] >= 1 && tracker.options[:teamcity_min_level] <= 3
-        Glue.fatal "min level should be between 1 to 3, not #{tracker.options[:teamcity_min_level]}"
+      unless tracker.options[:teamcity_min_level] < 1 || tracker.options[:teamcity-min-level] > 3
+        Glue.fatal "min level should be between 1 to 3, not #{tracker.options[:teamcity-min-level]}"
       end
       min_level = tracker.options[:teamcity_min_level]
     end
     reports = [ ]
 
-    output = ""
-
-    output << "##teamcity[message text='Report failed tests for each finding with severity equal or above #{printSeverity(min_level)}' status='NORMAL']" << "\n"
+    puts "##teamcity[message text='Report failed tests for each finding with severity equal or above #{printSeverity(min_level)}' status='NORMAL']"
 
     tracker.findings.group_by{|finding| finding.task}.each do |task, task_findings|
-      output << "##teamcity[testSuiteStarted name='#{task}']" << "\n"
+      puts "##teamcity[testSuiteStarted name='#{task}']"
       task_findings.each do |finding|
-        output << finding.severity
         if finding.severity < min_level
-          output << "##teamcity[testIgnored name='#{finding.fingerprint}' message='Severity #{printSeverity(finding.severity)}']" << "\n"
-          next
+          puts "##teamcity[testIgnored name='#{finding.fingerprint}' message='Severity #{printSeverity(finding.severity)}']"
+          return
         end
 
-        output << "##teamcity[testStarted name='#{finding.fingerprint}' captureStandardOutput='true']" << "\n"
-        output << "##teamcity[testFailed name='#{finding.fingerprint}' message='Severity #{printSeverity(finding.severity)}' details='#{finding.description}']" << "\n"
-        output << "Source: #{finding.source}" << "\n"
-        output << "##teamcity[testFinished name='#{finding.fingerprint}']" << "\n"
+        puts "##teamcity[testStarted name='#{finding.fingerprint}' captureStandardOutput='true']"
+        puts "##teamcity[testFailed name='#{finding.fingerprint}' message='Severity #{printSeverity(finding.severity)}' details='#{finding.description}']"
+        puts "Source: #{finding.source}"
+        puts "##teamcity[testFinished name='#{finding.fingerprint}']"
       end
-      output << "##teamcity[testSuiteFinished name='#{task}']" << "\n"
+      puts "##teamcity[testSuiteFinished name='#{task}']"
     end
-
-    return output
   end
 
   def out(finding)
