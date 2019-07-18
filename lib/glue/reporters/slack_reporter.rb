639e7b8e6c4ddd6bc7f628d20116fe79011ea25f
Matteo Palladino <pastnullinfinity@gmail.com>
2019-07-18 18:35:46 +0200

    started to work on more expressive slack reporter (not working)

--- lib/glue/reporters/slack_reporter.rb
+++ lib/glue/reporters/slack_reporter.rb
@@ -3,6 +3,15 @@ require 'glue/reporters/base_reporter'
 require 'jira-ruby'
 require 'slack-ruby-client'
 
+# In IRB
+# require 'slack-ruby-client'
+# Slack.configure do |config|
+#   config.token = "token"
+# end
+# client = Slack::Web::Client.new
+# client.chat_postMessage(channel: 'channel_name', text: "message_text", attachments: json_attachment, as_user: post_as_user)
+
+
 class Glue::SlackReporter < Glue::BaseReporter
   Glue::Reporters.add self
 
@@ -11,6 +20,34 @@ class Glue::SlackReporter < Glue::BaseReporter
   def initialize()
     @name = "SlackReporter"
     @format = :to_slack
+  end
+
+
+  def get_slack_attachment(finding)
+    json = {
+      "fallback": "Results of OWASP Glue test for repository #{tracker.options[:appname]}:",
+      "color": slack_priority(finding.severity),
+      "title": "#{finding.description}",
+      "title_link":"#{finding.detail}",
+      "text":"#{finding.source}"
+    }
+  end
+
+  def slack_priority(severity,default_priority=nil)
+    return default_priority="danger" if default_priority
+    if is_number?(severity)
+      f = Float(severity)
+      if f = 3
+        'warning'
+      elsif f = 2
+        'danger'
+      elsif f = 1
+        '#192022'
+      else
+        Glue.notify "**** Unknown severity type #{severity}"
+              
+        severity
+    end
   end
 
   def run_report(tracker)
@@ -40,13 +77,16 @@ class Glue::SlackReporter < Glue::BaseReporter
 
     reports = []
     tracker.findings.each do |finding|
-      reports << finding.to_string
+      begin
+        reports << get_slack_attachment(finding)
+      end
+      reports.join
     end
 
     puts tracker.options[:slack_channel]
 
     begin
-      client.chat_postMessage(channel: tracker.options[:slack_channel], text: "OWASP Glue test run completed - See attachment.", attachments: reports.join << "\n", as_user: post_as_user)
+      client.chat_postMessage(channel: tracker.options[:slack_channel], text: "OWASP Glue test run completed - See attachment.", attachments: reports.to_json << "\n", as_user: post_as_user)
     rescue Slack::Web::Api::Error => error
       Glue.fatal "Post to slack failed: " << error.to_s
     end
